6effee9ef7f49c0c7313ec981b0d1ca6f41bf472
Miguel Morin <32396311+miguelmorin@users.noreply.github.com>
2020-06-13 13:02:43 +0100

    refactored MPGN game processing

--- flask_app.py
+++ flask_app.py
@@ -23,6 +23,11 @@ global FILEPATH
 
 POSITION = "position"
 
+NOTES_KEY = "notes"
+HALF_MOVE_KEY = "half-move"
+PURE_MOVE_KEY = "move"
+FINISHED = "finished"
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -62,6 +67,11 @@ def load_macos_game(fp):
 def load_mpgn_game(fp):
     """Loads a game in my version of PGN.
     """
+    with open(fp) as f:
+        contents = f.read(-1)
+
+    # split into lines and return non-empty ones
+    return [l for l in contents.split("\n") if l]
 
 
 def load_game(fp):
@@ -111,12 +121,16 @@ def uci_moves_to_state(moves, current_line):
         move = chess.Move.from_uci(moves[i])
         san_history.append(board.san(move))
         board.push(move)
+        
     return board.fen(), san_history
 
 
 def san_history_to_html(san_history):
     """Converts a history of moves in Standard Algebraic Notation (SAN) to HTML that
     can be displayed.
+
+    This is for use for chess games from macOS, because those from MPGN have input like
+    ['0.0.e4', 'e5', '1.0.Bc4', 'Bc5', '2.0.Qh5', 'g6']
 
     >>> "1. e4 e5<br/>2. Nc3" == san_history_to_html(["e4", "e5", "Nc3"])
     True
@@ -137,12 +151,135 @@ def san_history_to_html(san_history):
     return "".join(output)
 
 
+def parse_mpgn_line(mpgn_line):
+    """Converts my notation of game to the corresponding details: half-move, pure
+    move, and notes.
+    
+    >>> res = parse_mpgn_line("1. e4")
+    >>> 0 == res[HALF_MOVE_KEY]
+    True
+    >>> "e4" == res[PURE_MOVE_KEY]
+    True
+    >>> res = parse_mpgn_line("1...e6 French Defence")
+    >>> 1 == res[HALF_MOVE_KEY]
+    True
+    >>> "French Defence" == res[NOTES_KEY]
+    True
+    >>> res = parse_mpgn_line("2...c5")
+    >>> 3 == res[HALF_MOVE_KEY]
+    True
+    >>> FINISHED == parse_mpgn_line("final comments")[HALF_MOVE_KEY]
+    True
+
+    """
+
+    res = dict()
+
+    # Maybe I should upgrade to regular expressions?
+    if not mpgn_line[0].isdigit():
+        res[NOTES_KEY] = mpgn_line
+        res[HALF_MOVE_KEY] = FINISHED
+        return res
+    
+    dot_index = mpgn_line.index(".")
+    move_number_str = mpgn_line[:dot_index]
+    
+    move_number = int(move_number_str)
+    white = ("." != mpgn_line[dot_index + 1])
+
+    res[HALF_MOVE_KEY] = move_number * 2 - white - 1
+
+    notes = ""
+    start_index = dot_index
+    while True:
+        start_index += 1
+        if mpgn_line[start_index] not in [".", " "]:
+            break
+    end_index = mpgn_line.find(" ", start_index)
+    if -1 == end_index:
+        pure_move = mpgn_line[start_index:]
+    else:
+        pure_move = mpgn_line[start_index:end_index]
+        notes = mpgn_line[(end_index + 1):]
+    
+    res[NOTES_KEY] = notes
+    res[PURE_MOVE_KEY] = pure_move
+
+    return res    
+
+
+def mpgn_to_mainline(mpgn, current_line):
+    """Finds the game line that leads to the current line.
+
+    >>> single_line = ["1. e4", "1...e5", "2. Nc3"]
+    >>> result, branch_length = mpgn_to_mainline(single_line, 2)
+    >>> result == single_line
+    True
+    >>> 3 == branch_length
+    True
+    >>> double_line = single_line + ["1...c5"]
+    >>> result, branch_length = mpgn_to_mainline(double_line, 3)
+    >>> result == ["1. e4", "1...c5"]
+    True
+    >>> 1 == branch_length
+    True
+    """
+    mainline = []
+    last_branch_length = 0
+    last_notes = ""
+    for source_line in range(1 + current_line):
+        line = mpgn[source_line]
+        half_move = parse_mpgn_line(line)[HALF_MOVE_KEY]
+        
+        if len(mainline) <= half_move:
+            last_branch_length += 1
+        else:
+            mainline = mainline[:half_move]
+            last_branch_length = 1
+        mainline.append(line)
+    return mainline, last_branch_length
+
+
+def mpgn_moves_to_state(mpgn_moves, current_line):
+
+    # Find mainline
+    mainline, last_branch_length = mpgn_to_mainline(mpgn_moves, current_line)
+
+    # TODO: add rewinding with length of last_branch_length
+
+    # Iterate on mainline to produce a game in SAN
+    last_notes = ""
+    finished = False
+    san_history = []
+    for i in range(len(mainline)):
+        details = parse_mpgn_line(mainline[i])
+        half_move = details[HALF_MOVE_KEY]
+        if FINISHED == half_move:
+            finished = True
+        else:
+            move = ""
+            if 0 == half_move % 2:
+                move += str(half_move / 2) + "."
+            move += details[PURE_MOVE_KEY]
+            san_history.append(move)
+        notes = details[NOTES_KEY]
+        if "" != notes:
+            last_notes = notes
+
+    pgn = io.StringIO(" " .join(san_history))
+    game = chess.pgn.read_game(pgn)
+    board = game.board()
+    for move in game.mainline_moves():
+        board.push(move)
+    
+    return board.fen(), san_history, last_notes
+
+
 def state():
     """Returns the state of the board from the global variables CURRENT_LINE and FILEPATH.
     """
 
     moves = load_game(FILEPATH)
-    print(CURRENT_LINE)
 
     if len(moves) <= CURRENT_LINE:
         print("at end of game!")
@@ -155,12 +292,17 @@ def state():
         print(san_html)
         return "\n".join([POSITION, fen, san_html])
     else:
-        assert False
+        fen, san_history, last_notes = mpgn_moves_to_state(moves, CURRENT_LINE)
+        san_html = san_history_to_html(san_history)
+        print(san_history)
+        print(san_html)
+        return "\n".join([POSITION, fen, san_html, last_notes])
     
 
 # TODO: refactor this into a state function, so it works well
 # with CURRENT_LINE = 22
 
+"""
 def next_move(increment = 1):
     global CURRENT_LINE, GAME_MOVES, MOVE_MAINLINE, REWOUND
 
@@ -174,30 +316,9 @@ def next_move(increment = 1):
         return "\n".join([action, line])
 
 
-    # Compute the place of this half-move
-    dot_index = line.index(".")
-    move_number_str = line[:dot_index]
-    if not move_number_str.isdigit():
-        return "finished\n" + GAME_MOVES[CURRENT_LINE]
-    
-    move_number = int(move_number_str)
-    white = ("." != line[dot_index + 1])
 
     # Find the pure move and notes
     start_index = dot_index
-    notes = ""
-    while True:
-        start_index += 1
-        if line[start_index] not in [".", " "]:
-            break
-    end_index = line.find(" ", start_index)
-    if -1 == end_index:
-        pure_move = line[start_index:]
-    else:
-        pure_move = line[start_index:end_index]
-        notes = line[(end_index + 1):]
-
-    move_place = move_number * 2 - white - 1
 
     print("Current game line = %d, line = %s, move_place = %d " % (CURRENT_LINE, line, move_place))
     
@@ -234,7 +355,7 @@ def next_move(increment = 1):
     if notes:
         answer.append(notes)
     return "\n".join(answer)
-                     
+"""                     
 
 @app.route('/next')
 def next():
@@ -265,6 +386,12 @@ def test_get(tester):
 
 
 if __name__ == '__main__':
+
+    single_line = ["1. e4", "1...e5", "2. Nc3"]
+    double_line = single_line + ["1...c5"]
+    result, branch_length = mpgn_to_mainline(double_line, 3)
+    print(result)
+    print(branch_length)
 
     doctests = doctest.testmod()
     assert 0 == doctests.failed, "Some doc-tests failed, exiting..."
